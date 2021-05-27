5b56d317e7ad130dc3a1f6fdb95221ff84320c13
juacarchi <cardenasfanoce@gmail.com>
2021-05-27 13:08:23 +0200

    Actualizacion

--- JuegoEstaciones/Assets/Scripts/GameplayManager.cs
+++ JuegoEstaciones/Assets/Scripts/GameplayManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,32 +6,32 @@ public class GameplayManager : MonoBehaviour
 {
     //public GameObject[] prefabInstanciado;
     public GameObject canvasPadre;
-    public List<GameObject> posObjectsInPanel;
+    public List<RectTransform> posObjectsInPanel;
     public GameObject panelVictory;
     public int n_estacion;
     Estacion newEstacion;
+
     GameObject[] correctObjects;
     GameObject[] incorrectObjects;
+
     public Text textPrincipal;
     Text textEstacion;
-    public GameObject posTextoPrincipal;
-    public List<GameObject> backgroundsController;
+
     public int n_aciertos;
     public Image BG;
-    public Sprite SpriteBg;
-    SFXManager2 sonidoVictoria;
+    
     public GameObject panelReiniciar;
     GameManager nivelCompletado;
 
-    List <GameObject> estacionesList;
+    List<GameObject> estacionesList;
 
     // Start is called before the first frame update
     void Start()
     {
-        sonidoVictoria = GameObject.Find("SFXManager2").GetComponent<SFXManager2>();
+        
         nivelCompletado = GameObject.Find("GameManager").GetComponent<GameManager>();
         n_aciertos = 0;
-       panelVictory.SetActive(false);
+        panelVictory.SetActive(false);
         panelReiniciar.SetActive(false);
         estacionesList = GameManager.instance.GetEstacionList();
         RandomEstacion();
@@ -44,49 +43,51 @@ public class GameplayManager : MonoBehaviour
         newEstacion = estacionesList[n_estacion].GetComponent<Estacion>();
         correctObjects = newEstacion.GetCorrectsObjects();
         incorrectObjects = newEstacion.GetIncorrectObjects();
-        textEstacion = newEstacion.GetTextEstacion();
-        textPrincipal.text = textEstacion.text;
-        SpriteBg = newEstacion.GetSpriteFondo();
-        BG.sprite = SpriteBg;
+        textPrincipal.text = newEstacion.GetNameEstacion();
+        BG.sprite = newEstacion.GetSpriteFondo();
         nivelCompletado.levelsCompleted += 1;
 
         for (int i = 0; i < correctObjects.Length; i++)
         {
             int aleatorio = Random.Range(0, posObjectsInPanel.Count);
-            GameObject instanciarCorrecto = Instantiate(correctObjects[i], posObjectsInPanel[aleatorio].transform.position, Quaternion.identity);
-            instanciarCorrecto.transform.SetParent(canvasPadre.transform);
+            GameObject instanciarCorrecto = Instantiate(correctObjects[i], posObjectsInPanel[aleatorio].anchoredPosition, Quaternion.identity);
+            instanciarCorrecto.transform.SetParent(posObjectsInPanel[aleatorio]);
+            RectTransform rect = instanciarCorrecto.GetComponent<RectTransform>();
+            rect.anchoredPosition = Vector3.zero;
+            rect.localScale = new Vector2(1, 1);
             posObjectsInPanel.RemoveAt(aleatorio);
             Button botonProv = instanciarCorrecto.gameObject.GetComponent<Button>();
             botonProv.interactable = true;
         }
         for (int i = 0; i < incorrectObjects.Length; i++)
         {
-            Button botonProv = incorrectObjects[i].gameObject.GetComponent<Button>();
-            botonProv.interactable = true;
-            int aleatorio2 = Random.Range(0, posObjectsInPanel.Count);
-            GameObject instanciarIncorrecto = Instantiate(incorrectObjects[i], posObjectsInPanel[aleatorio2].transform.position, Quaternion.identity);
-            instanciarIncorrecto.transform.SetParent(canvasPadre.transform);
-            posObjectsInPanel.RemoveAt(aleatorio2);
+            int aleatorio = Random.Range(0, posObjectsInPanel.Count);
+            GameObject instanciarIncorrecto = Instantiate(incorrectObjects[i], posObjectsInPanel[aleatorio].anchoredPosition, Quaternion.identity);
+            instanciarIncorrecto.transform.SetParent(posObjectsInPanel[aleatorio]);
+            RectTransform rect = instanciarIncorrecto.GetComponent<RectTransform>();
+            rect.anchoredPosition = Vector3.zero;
+            rect.localScale = new Vector2(1, 1);
+            posObjectsInPanel.RemoveAt(aleatorio);
             Button botonProv2 = instanciarIncorrecto.gameObject.GetComponent<Button>();
             botonProv2.interactable = false;
         }
         estacionesList.RemoveAt(n_estacion);
-        
+
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
-       if(n_aciertos == 5)
+        if (n_aciertos == 5)
         {
             panelVictory.SetActive(true);
-            SFXManager2.instance.PlaySFX(sonidoVictoria.victory);
+            SFXManager2.instance.PlaySFX(SFXManager2.instance.victory);
             //SFXManager2.instance.PlaySFX(sonidoVictoria.victory);
             n_aciertos = 0;
             if (nivelCompletado.levelsCompleted == 4)
             {
-                SFXManager2.instance.PlaySFX(sonidoVictoria.victory);
+                SFXManager2.instance.PlaySFX(SFXManager2.instance.victory);
                 panelVictory.SetActive(false);
                 panelReiniciar.SetActive(true);
                 for (int i = 0; i < GameManager.instance.principalEstacionList.Count; i++)
